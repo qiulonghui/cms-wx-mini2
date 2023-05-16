dd12c5facc2bb9436f5c6d246429776cd4894a2a
qiulh <569374939@qq.com>
2023-05-17 01:02:45 +0800

    20230517

--- miniprogram/pages/order/order.js
+++ miniprogram/pages/order/order.js
@@ -1,22 +1,48 @@
 // pages/order/order.js
+import Toast from '@vant/weapp/toast/toast';
 const app = getApp()
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    id: null,
+    action: 'create',
+    pageTitle: '',
+    name: "",
+    phone: "",
+    depart: "",
+    address: "",
+    desc: ""
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
   onLoad(options) {
-
+    this.data.id = options.id
+    this.data.action = options.action
+    let title
+    if (this.data.action === 'create') {
+      title = '新建报修'
+    }else{
+      title = '修改工单'
+    }
+    this.setData({
+      pageTitle: title
+    })
   },
 
   onClickVantBack() {
     wx.navigateBack()
+  },
+  isNullValue(value) {
+    if (value === '' || value == null) return true
+    return false
+  },
+  onClickSaveBtn() {
+    const name = this.data.name.trim()
+    const phone = this.data.phone.trim()
+    const depart = this.data.depart.trim()
+    const address = this.data.address.trim()
+    const desc = this.data.desc.trim()
+    const flag = this.isNullValue(name) || this.isNullValue(phone) || this.isNullValue(depart) || this.isNullValue(address) || this.isNullValue(desc)
+    if (flag) {
+      Toast('请将信息填写完整');
+      return
+    }
   }
 })
--- miniprogram/pages/orderList/orderList.js
+++ miniprogram/pages/orderList/orderList.js
@@ -1,8 +1,15 @@
 Page({
-  data: {},
+  data: {
+    orderList:[1,2,3,4,5]
+  },
   toCreateOrder() {
     wx.navigateTo({
-      url: '/pages/order/order'
+      url: '/pages/order/order?action=create'
+    })
+  },
+  onClickCard() {
+    wx.navigateTo({
+      url: '/pages/order/order?id=1&action=edit'
     })
   }
 });
